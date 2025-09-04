9dca935bc08fac5bfa8f27eca228ac2c2931e28f
Никита Войтов <voitov@MacBook-Air-Nikita.local>
2025-09-04 12:53:48 +0300

    step-5

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
 
 def index(request):
     return render(
@@ -8,3 +10,7 @@ def index(request):
             "app_name": "Статьи",
         },
     )
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('article')
--- hexlet_django_blog/views.py
+++ hexlet_django_blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
 def index(request):
@@ -12,3 +13,11 @@ def index(request):
 
 def about(request):
     return render(request, 'about.html')
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['who'] = 'World'
+        return context
